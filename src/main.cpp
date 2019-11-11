2205a6018d3f5b3198b300a3f82ab18e23938e94
Paul Smith <code@uvwxy.de>
2019-11-11 19:56:03 +0100

    add hue animation

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@
 #include <FastLED.h>
 #define LEDS_0_PIN 4
 #define LEDS_1_PIN 14
-#define LEDS_0_NUM 50
-#define LEDS_1_NUM 50
+#define LEDS_0_NUM 46
+#define LEDS_1_NUM 46
 #define MAX_BRIGHTNESS 255
 #define DIST 6
 #define OFFSET 5
@@ -18,17 +18,17 @@ CRGB leds1[LEDS_1_NUM];
 #define NUM_BARS 7
 uint8_t bars0[] = {4, 11, 17, 23, 30, 36, 42};
 uint8_t bars1[] = {4, 10, 16, 22, 29, 35, 41};
+uint16_t h[] = {0, 20, 40, 60, 80, 100, 120};
+uint8_t hR;  // hue dim value
+uint8_t hG;
+uint8_t hB;
+unsigned long lastChanged = 0;
+unsigned long changeInterval = 1000;
 
 #define FLASH_BUTTON_PIN 0
 #define INTERNAL_LED 2  // the tiny blue led on the esp8285
 #define LED_ON LOW
 #define LED_OFF HIGH
-
-uint8_t state = 1;
-#define MAX_STATES 4
-uint8_t globalRed;
-uint8_t globalGreen;
-uint8_t globalBlue;
 
 ESP8266WebServer server(80);
 
@@ -55,6 +55,52 @@ void checkWifi() {
   }
 }
 
+void setLedColorHSV(byte h, byte s, byte v) {
+  // this is the algorithm to convert from RGB to HSV
+  h = (h * 192) / 256;            // 0..191
+  unsigned int i = h / 32;        // We want a value of 0 thru 5
+  unsigned int f = (h % 32) * 8;  // 'fractional' part of 'i' 0..248 in jumps
+
+  unsigned int sInv = 255 - s;  // 0 -> 0xff, 0xff -> 0
+  unsigned int fInv = 255 - f;  // 0 -> 0xff, 0xff -> 0
+  byte pv = v * sInv / 256;     // pv will be in range 0 - 255
+  byte qv = v * (256 - s * f / 256) / 256;
+  byte tv = v * (256 - s * fInv / 256) / 256;
+
+  switch (i) {
+    case 0:
+      hR = v;
+      hG = tv;
+      hB = pv;
+      break;
+    case 1:
+      hR = qv;
+      hG = v;
+      hB = pv;
+      break;
+    case 2:
+      hR = pv;
+      hG = v;
+      hB = tv;
+      break;
+    case 3:
+      hR = pv;
+      hG = qv;
+      hB = v;
+      break;
+    case 4:
+      hR = tv;
+      hG = pv;
+      hB = v;
+      break;
+    case 5:
+      hR = v;
+      hG = pv;
+      hB = qv;
+      break;
+  }
+}
+
 void setColor(CRGB leds[], uint8_t r, uint8_t g, uint8_t b, uint16_t len) {
   for (int x = 0; x < len; x++) {
     leds[x] = CRGB(r, g, b);
@@ -67,7 +113,8 @@ void setStrandColors(CRGB leds[], uint8_t bars[], uint16_t num, uint8_t r, uint8
       if (bar) {
         if (abs(x - bars[i]) <= 0) {
           // on a bar
-          leds[x] = CRGB(r, g, b);
+          setLedColorHSV(h[i], 255, 255);  // calculate hR,hG,hB...
+          leds[x] = CRGB(hR, hG, hB);
         }
       } else {
         if (abs(x - bars[i]) > 0) {
@@ -87,45 +134,12 @@ void setStrand1(uint8_t r, uint8_t g, uint8_t b, boolean bar) {
   setStrandColors(leds1, bars1, LEDS_1_NUM, r, g, b, bar);
 };
 
-void updateState() {
-  switch (state) {
-    case 0:
-      globalRed = 0;
-      globalGreen = 0;
-      globalBlue = 0;
-      break;
-    case 2:
-      globalRed = 255;
-      globalGreen = 0;
-      globalBlue = 0;
-      break;
-    case 3:;
-      globalRed = 0;
-      globalGreen = 255;
-      globalBlue = 0;
-      break;
-    case 4:
-      globalRed = 0;
-      globalGreen = 0;
-      globalBlue = 255;
-      break;
-    case 1:
-    default:
-      globalRed = 255;
-      globalGreen = 255;
-      globalBlue = 255;
-  }
-}
-
 void setup() {
   Serial.begin(115200);
 
   pinMode(INTERNAL_LED, OUTPUT);
   pinMode(FLASH_BUTTON_PIN, INPUT);
 
-  updateState();
-  setColor(leds0, globalRed, globalGreen, globalBlue, LEDS_0_NUM);
-  setColor(leds1, globalRed, globalGreen, globalBlue, LEDS_1_NUM);
   LEDS.addLeds<WS2812, LEDS_0_PIN, RGB>(leds0, LEDS_0_NUM);
   LEDS.addLeds<WS2812, LEDS_1_PIN, RGB>(leds1, LEDS_1_NUM);
   LEDS.setBrightness(MAX_BRIGHTNESS);
@@ -143,38 +157,15 @@ void setup() {
     server.send(300, "text/plain", String("Error. Input 0 - 255"));
   });
 
-  server.on("/toggle_0", [&]() {
-    int v = atoi(server.arg("value").c_str());
-    if (v >= 0 && v < LEDS_0_NUM) {
-      Serial.print("Toggling led0 value");
+  server.on("/speed", [&]() {
+    uint v = atoi(server.arg("value").c_str());
+    if (v >= 0 && v < 10000) {
+      Serial.print("Set speed ");
       Serial.println(v);
 
-      if (leds0[v].r + leds0[v].g + leds0[v].b == 0) {
-        leds0[v] = CRGB(globalRed, globalGreen, globalBlue);
-      } else {
-        leds0[v] = CRGB(0, 0, 0);
-      }
+      changeInterval = v;
 
-      LEDS.show();
-      server.send(200, "text/plain", String("Set value to ") + String(v));
-    }
-    server.send(300, "text/plain", String("Input error"));
-  });
-
-  server.on("/toggle_1", [&]() {
-    int v = atoi(server.arg("value").c_str());
-    if (v >= 0 && v < LEDS_1_NUM) {
-      Serial.print("Toggling led1 value");
-      Serial.println(v);
-
-      if (leds1[v].r + leds1[v].g + leds1[v].b == 0) {
-        leds1[v] = CRGB(globalRed, globalGreen, globalBlue);
-      } else {
-        leds1[v] = CRGB(0, 0, 0);
-      }
-
-      LEDS.show();
-      server.send(200, "text/plain", String("Set value to ") + String(v));
+      server.send(200, "text/plain", String("Set speed to ") + String(v));
     }
     server.send(300, "text/plain", String("Input error"));
   });
@@ -190,22 +181,24 @@ void loop() {
 
   if (button == LOW) {
     digitalWrite(INTERNAL_LED, LED_ON);
-    state++;
-    state = state > MAX_STATES ? 0 : state;
-    Serial.println(state);
-    Serial.println(globalRed);
-    Serial.println(globalGreen);
-    Serial.println(globalBlue);
-
-    updateState();
-    setStrand0(32, 32, 32, false); // wall
-    setStrand0(globalRed, globalGreen, globalBlue, true); // bar
-
-    setStrand1(32, 32, 32, false); // wall
-    setStrand1(globalRed, globalGreen, globalBlue, true); // bar
-
-    LEDS.show();
     delay(500);  // debounce / rate limit
+  }
+
+  if (millis() - lastChanged > changeInterval) {
+    for (uint8_t i = 0; i < NUM_BARS; i++) {
+      h[i]++;
+    }
+    setStrand0(32, 32, 32, false);  // wall
+    setStrand0(0, 0, 0, true);      // bar
+
+    setStrand1(32, 32, 32, false);  // wall
+    setStrand1(0, 0, 0, true);      // bar
+
+    // I borked the last led
+    leds1[45] = CRGB(0, 0, 0);
+    LEDS.show();
+
+    lastChanged = millis();
   }
 
   delay(5);
